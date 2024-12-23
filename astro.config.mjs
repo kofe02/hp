2ee5abeba82d55e890e0d76c48bab2b19b57eb31
kofe <t2312121@gl.cc.uec.ac.jp>
2024-12-24 00:33:10 +0900

    サイトURLとベースパスを更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://kofe02.github.io",
+  base: "/hp",
   trailingSlash: "always",
   integrations: [
     tailwind(
